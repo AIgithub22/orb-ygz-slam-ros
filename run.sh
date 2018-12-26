0991a483ac49a64844f84947e6b60d808adb5929
jqh <229331189@qq.com>
2018-12-26 02:46:54 -0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./Examples/Monocular/mono_euroc_vins Vocabulary/ORBvoc.bin Examples/Monocular/jpeuroc.yaml /media/raymond/OS/MH_01/cam0/data /home/raymond/Downloads/ORB-YGZ-SLAM/Examples/Monocular/EuRoC_TimeStamps/MH01.txt /media/raymond/OS/MH_01/imu0/data.csv  
+./Examples/Monocular/mono_euroc_vins Vocabulary/ORBvoc.bin Examples/Monocular/jpeuroc.yaml /home/ubuntu/Desktop/stereo+inertial+slam+datasets/MH_01_easy/mav0/cam0/data /home/ubuntu/ORB-YGZ-SLAM/Examples/Monocular/EuRoC_TimeStamps/MH01.txt /home/ubuntu/Desktop/stereo+inertial+slam+datasets/MH_01_easy/mav0/imu0/data.csv  
 
