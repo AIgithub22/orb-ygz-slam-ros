2b2477bca10476a6ad0fd07bd4ac97f4385c277f
raymond.c.liang@outlook.com <raymond.c.liang@outlook.com>
2018-12-26 17:55:11 +0800

    ADD YAML of JP

---
+++ run.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular/mono_euroc_vins Vocabulary/ORBvoc.bin Examples/Monocular/jpeuroc.yaml /media/raymond/OS/MH_01/cam0/data /home/raymond/Downloads/ORB-YGZ-SLAM/Examples/Monocular/EuRoC_TimeStamps/MH01.txt /media/raymond/OS/MH_01/imu0/data.csv  
+
