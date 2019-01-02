3f8b1dae97658484f37c9e14e57db20a8a9e9c44
jqh <229331189@qq.com>
2019-01-01 21:28:01 -0800

    change ROSpackage

---
+++ ROS/ORB_YGZ_VIO/build/gtest/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/ubuntu/ORB-YGZ-SLAM/ROS/ORB_YGZ_VIO/lib/libgtest.pdb"
+  "/home/ubuntu/ORB-YGZ-SLAM/ROS/ORB_YGZ_VIO/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
